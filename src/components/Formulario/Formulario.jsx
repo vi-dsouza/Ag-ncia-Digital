cd71e1310b45c639934bc2dcc1f35116a206f259
Vitória Stéfane de Souza <126826913+vi-dsouza@users.noreply.github.com>
2025-06-04 15:22:57 -0300

    incluindo rota back Formulario.jsx

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -19,7 +19,7 @@ const Formulario = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const response = await fetch("http://localhost:5000/enviar-email",  {
+        const response = await fetch("https://backend-da-agencia.vercel.app",  {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData),
