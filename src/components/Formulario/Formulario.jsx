6f39021d35f0db8471f557a5b437a8138208a9f0
Vitória Stéfane de Souza <126826913+vi-dsouza@users.noreply.github.com>
2025-06-04 15:27:08 -0300

    Update Formulario.jsx

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -19,7 +19,7 @@ const Formulario = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const response = await fetch("https://backend-da-agencia.vercel.app",  {
+        const response = await fetch("https://backend-da-agencia.vercel.app/enviar-email",  {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData),
